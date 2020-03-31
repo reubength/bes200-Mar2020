bfe79daf91e50e2232dc1478b6a12414de196135
reubength <reubength@gmail.com>
2020-03-31 15:21:15 -0400

    Day 1 BES200 commit

---
+++ LibraryApiIntegrationTests/ResourceSmokeTests.cs
@@ -0,0 +1,83 @@
+﻿using LibraryApi;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LibraryApiIntegrationTests
+{
+    public class ResourceSmokeTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient Client;
+
+        public ResourceSmokeTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            Client = factory.CreateClient();
+        }
+
+        [Theory]
+        [InlineData("/books")]
+        [InlineData("/books/1")]
+        public async Task CheckItResourceIsAlive(string resource)
+        {
+            var response = await Client.GetAsync(resource);
+
+            Assert.True(response.IsSuccessStatusCode);
+
+        }
+
+        [Fact]
+        public async Task CanGetBook()
+        {
+            var response = await Client.GetAsync("/books/1");
+
+            var book = await response.Content.ReadAsAsync<BookDetailsResponse>();
+
+            Assert.Equal(1, book.id);
+            Assert.Equal("Walden", book.title);
+            Assert.Equal("Philosophy", book.genre);
+        }
+        [Fact]
+        public async Task CanAddBook()
+        {
+            var bookToAdd = new PostBookRequest
+            {
+                title = "Composing Electronic Music",
+                author = "Smihht",
+                genre = "Non-Fiction",
+                numberOfPages = 300
+            };
+
+            var response = await Client.PostAsJsonAsync("/books", bookToAdd);
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+
+            var location = response.Headers.Location.LocalPath;
+            var getItResponse = await Client.GetAsync(location);
+            var responseData = await getItResponse.Content.ReadAsAsync<BookDetailsResponse>();
+
+            Assert.Equal(bookToAdd.title, responseData.title);
+            Assert.Equal(bookToAdd.author, responseData.author);
+
+        }
+    }
+    public class BookDetailsResponse
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public string genre { get; set; }
+        public int numberOfPages { get; set; }
+    }
+
+    public class PostBookRequest
+    {
+        public string title { get; set; }
+        public string author { get; set; }
+        public string genre { get; set; }
+        public int numberOfPages { get; set; }
+    }
+
+}
---
+++ LibraryApiIntegrationTests/UnitTest1.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+
+namespace LibraryApiIntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var a = 10;
+            var b = 10;
+
+            Assert.Equal(20, a + b);
+        }
+
+        [Theory]
+        [InlineData(2, 2, 4)]
+        [InlineData(10, 5, 15)]
+        [InlineData(20, 3, 23)]
+        public void CanAddTwoNumbers(int a, int b, int expected)
+        {
+            var sum = a + b;
+            // Jeff
+            Assert.Equal(expected, sum);
+        }
+
+    }
+}
